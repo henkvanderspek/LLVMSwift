f3cb5380e761781a9e6b5015d3b77725fa937da6
Henk van der Spek <henk@vanmezelf.nl>
2022-01-22 16:37:31 +0100

    Struct test

--- jit/jitApp.swift
+++ jit/jitApp.swift
@@ -18,38 +18,51 @@ struct jitApp: App {
     }
 }
 
+@_cdecl("_print")
+func print()->Int64 {
+    print("Test")
+    return 20
+}
+
 private extension jitApp {
     func run() {
+        let RTLD_DEFAULT = UnsafeMutableRawPointer(bitPattern: -2)
         let m = Module(name: "main")
         let b = IRBuilder(module: m)
         let main = b.addFunction("main", type: .init([], IntType.int64))
         let e = main.appendBasicBlock(named: "entry")
         b.positionAtEnd(of: e)
-        let foo = b.addFunction("foo", type: .init([], IntType.int64))
-        let r = b.buildCall(foo, args: [])
+        let type = b.createStruct(name: "DataType", types: [IntType.int64, IntType.int64], isPacked: false)
+        let mem = b.buildAlloca(type: type)
+        let field0 = b.buildInBoundsGEP(mem, type: type, indices: [
+            IntType.int32.constant(0),
+            IntType.int32.constant(0)
+        ])
+        let field1 = b.buildInBoundsGEP(mem, type: type, indices: [
+            IntType.int32.constant(0),
+            IntType.int32.constant(1)
+        ])
+        b.buildStore(IntType.int64.constant(20), to: field0)
+        let val0 = b.buildLoad(field0, type: IntType.int64)
+        b.buildStore(IntType.int64.constant(10), to: field1)
+        let val1 = b.buildLoad(field1, type: IntType.int64)
+        let pr = b.addFunction("print", type: .init([], IntType.int64))
+        let r = b.buildCall(pr, args: [])
         // This would cause an infinite loop and stack overflow
         // let _ = b.buildCall(main, args: [])
-        let c = IntType.int64.constant(21)
+        let total = b.buildAdd(val0, val1)
+        let c = IntType.int64.constant(20)
         let s = b.buildAdd(r, c)
-        b.buildRet(s)
+        let ret = b.buildAdd(s, total)
+        b.buildRet(ret)
 
         do {
             let jit = try JIT(machine: TargetMachine())
             
             _ = try jit.addEagerlyCompiledIR(m) { (name) -> JIT.TargetAddress in
-                switch name {
-                case "_foo":
-                    typealias FooPtr = @convention(c) ()->Int64
-                    let f: FooPtr = {
-                        print("bar")
-                        return 100
-                    }
-                    let p: UnsafeRawPointer? =  unsafeBitCast(f, to: UnsafeRawPointer.self)
-                    let a = UInt64(bitPattern: Int64(Int(bitPattern: p)))
-                    return .init(raw: a)
-                default:
-                    return JIT.TargetAddress()
-                }
+                let sym = dlsym(RTLD_DEFAULT, name)
+                let a = UInt64(bitPattern: Int64(Int(bitPattern: sym)))
+                return a != 0 ? .init(raw: a) : .init()
             }
             
             typealias FnPtr = @convention(c) ()->Int64
