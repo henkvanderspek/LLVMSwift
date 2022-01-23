3bde8c354b10536dc0bb95413e952bc4d35c28dc
Henk van der Spek <henk@vanmezelf.nl>
2022-01-23 13:42:04 +0100

    Fixed bug in accessing parse buffer

--- Sources/LLVM/Parser.swift
+++ Sources/LLVM/Parser.swift
@@ -12,13 +12,14 @@ public final class Parser {
     self.context = context
   }
   
-  public func parse(source: String, name: String) -> Module {
-    let buffer = source.utf8CString.withUnsafeBufferPointer { $0 }
-    let buf = MemoryBuffer(buffer: buffer, name: UUID().uuidString).llvm
-    let mod = Module(name: name, context: context)
-    var module: LLVMModuleRef? = mod.llvm
-    var message: UnsafeMutablePointer<Int8>?
-    LLVMParseIRInContext(context.llvm, buf, &module, &message)
-    return mod
+  public func parse(source: String, name: String) -> Module? {
+    return source.utf8CString.withUnsafeBufferPointer { buffer in
+      let buf = MemoryBuffer(buffer: buffer, name: UUID().uuidString).llvm
+      let mod = Module(name: name, context: context)
+      var module: LLVMModuleRef? = mod.llvm
+      var message: UnsafeMutablePointer<Int8>?
+      LLVMParseIRInContext(context.llvm, buf, &module, &message)
+      return mod
+    }
   }
 }
